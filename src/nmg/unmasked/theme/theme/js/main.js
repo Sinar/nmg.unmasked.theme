095f86ccbe346b1dc04e4255de7bb514da8034dc
Khairil Yusof <khairil.yusof@gmail.com>
2020-09-01 17:10:23 +0800

    Initial theme based on OGP theme

---
+++ src/nmg/unmasked/theme/theme/js/main.js
@@ -0,0 +1,9 @@
+//Searchbox dropdown toggle
+function toggleSearchBox() {
+  let searchBox = document.getElementById("search-box");
+  if (searchBox.style.display === "block") {
+    searchBox.style.display = "none";
+  } else {
+    searchBox.style.display = "block";
+  }
+}
